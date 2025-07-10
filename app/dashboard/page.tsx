5b3baee379064c073185049f9db129c3540e9323
PyConqueror <wanaqim@gmail.com>
2025-07-10 11:02:04 +0800

    feat: add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div className="w-full">
+      <StudentsPage status="active" />
+    </div>
+  )
+}
+
+import StudentsPage from "@/components/students-page"
